3eb2e153dfe0b61fac684e09743805b3f5525de7
junaidhkn <junaidhkniazi@gmail.com>
2023-07-23 15:49:00 +0500

    hybrid renderering

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,11 @@ import react from "@astrojs/react";
 import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
-export default defineConfig({
-  integrations: [react(), solidJs()]
-});
+export default defineConfig( {
+  site: 'https://astro-frontend-masters.netlify.app/',
+  integrations: [react(), solidJs()],
+  output: 'hybrid',
+  experimental: {
+    hybridOutput: true
+  }
+} );
